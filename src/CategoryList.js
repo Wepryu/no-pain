da3a70238f66ed0c97395ea48faf3cffc36076d4
Wepryu <kocabas.oguzhan1@ogr.sakarya.edu.tr>
2019-08-27 16:14:46 +0300

    How to work wits statement on react components

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -1,18 +1,26 @@
 import React, { Component } from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 export default class CategoryList extends Component {
- 
+  constructor(props) {
+    super(props);
+    this.state = {
+      CategoryLst: [
+        { CategoryId: 0, CategoryName: "Kırmızı" },
+        { CategoryId: 1, CategoryName: "Beyaz" },
+        { CategoryId: 2, CategoryName: "Mavi" }
+      ]
+    };
+  }
   render() {
     return (
       <div>
         <h2>{this.props.infos.title}</h2>
         <h1>{this.props.infos.baskaBirOzellik}</h1>
         <ListGroup>
-          <ListGroupItem>Kırmızı</ListGroupItem>
-          <ListGroupItem>Beyaz</ListGroupItem>
-          <ListGroupItem>Siyah</ListGroupItem>
-          <ListGroupItem>Kahverengi</ListGroupItem>
-          <ListGroupItem>Mavi</ListGroupItem>
+            {this.state.CategoryLst.map(prmCategory =>(
+                <ListGroupItem key={prmCategory.CategoryId}>{prmCategory.CategoryName}</ListGroupItem>
+            ))}
+          
         </ListGroup>
       </div>
     );
